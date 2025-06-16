36bf0c5c763b78077e9df88a7446947cf2944a2d
Fedor_ex16@student.itstep.org <Fedor_ex16@student.itstep.org>
2025-06-16 17:43:44 +0300

    Добавьте файлы проекта.

---
+++ At_day,+at_night/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace At_day__at_night
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int num;
+            Console.WriteLine("number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 6; i++)
+            {
+                int d = num % 10;
+                num /= 10;
+                Console.Write(d);
+            }*/
+
+            //3
+            /*
+            int numStart;
+            int numEnd;
+            numStart = Convert.ToInt32(Console.ReadLine());
+            numEnd = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = numStart; i < numEnd; i++)
+            {
+                int sum = i-1 + i;
+
+                if (i+1 == sum)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            */
+
+            //7
+            int N;
+            Console.WriteLine("number: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(i);
+            }
+
+        }
+
+    }
+}
