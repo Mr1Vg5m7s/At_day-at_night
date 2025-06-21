e114b08d3cba0bac3576dc3a2fe03d4a44b4f080
Fedor_ex16@student.itstep.org <Fedor_ex16@student.itstep.org>
2025-06-21 17:50:29 +0300

    #2 les

--- At_day,+at_night/Program.cs
+++ At_day,+at_night/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace At_day__at_night
 {
@@ -6,6 +8,153 @@ namespace At_day__at_night
     {
         static void Main(string[] args)
         {
+            //int[] arr = new int[10];
+            //int[] arr1 = {2,2,2];
+            //int[] arr2;
+            //arr2 = new int[] { 1, 2};
+            //333333333333333333333333333333333333333333333333333333333333333333333333333
+            /*int[] arr1 = new int[9] { 1, 2, 3, 4, 5, 6, 1, 2, 3 };
+            int num1,num2,num3;
+            Console.WriteLine("number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            int Count = 0;
+            for (int i = 0; i < arr1.Length; i++) 
+            {
+                if (arr1[i] == num1 && arr1[i+1] == num2 && arr1[i+2] == num3)
+                {
+                    Count ++;
+                }
+            }
+            Console.WriteLine(Count);*/
+            //444444444444444444444444444444444444444444444444444444444444444444444444444
+            /*
+            Random random = new Random();
+
+            int size1 = 5;
+            int size2 = 6;
+            int[] arr1 = new int[size1];
+            int[] arr2 = new int[size2];
+            for (int i = 0; i < size1; i++)
+            {
+                arr1[i] = random.Next(1, 10);
+                Console.Write(arr1[i] + " ");
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < size2; i++)
+            {
+                arr2[i] = random.Next(1, 10);
+                Console.Write(arr2[i]+ " ");
+            }
+
+            int[] arr3;
+
+
+            for (int i = 0;i < size1; i++)
+            {
+                for (int j = 0; j < size2; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                       arr3 = new int[size1 + size2];
+                        arr3[i] = arr1[i];
+                        arr3[j] = arr2[j];
+                        Console.WriteLine(" ");
+                        Console.WriteLine("elements: " + arr3[i]);
+                    }
+                }
+            }*/
+            //55555555555555555555555555555555555555555555555555555555
+            /*
+            int[,] Arr1 = new int[5,5];
+            Random random = new Random();
+            for (int i = 0; i < Arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < Arr1.GetLength(1); j++)
+                {
+                    Arr1[i, j] = random.Next(1, 100);
+                    Console.Write(Arr1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" ");
+            int min = Arr1[0, 0];
+            int max = Arr1[0, 0];
+            for (int i = 0; i < Arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < Arr1.GetLength(1); j++)
+                {
+                    if (Arr1[i, j] < min)
+                    {
+                        min = Arr1[i, j];
+                    }
+                    if (Arr1[i, j] > max)
+                    {
+                        max = Arr1[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            */
+            //777777777777777777777777777777777777777777777777
+            string? st = "First Rule of the Fight club";
+            Console.WriteLine(st.Split(" ",StringSplitOptions.RemoveEmptyEntries).Count());
+            var words = st.Split();
+            for (int i = 0; i < st.Length; i++)
+            {
+                words[i] = new string(words[i].Reverse().ToArray());
+            }
+            Console.WriteLine(String.Join(" ", words));
+
+            /*
+            Random random = new Random();
+            int[] arr = new int[10];
+
+            int num;
+            Console.WriteLine("number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(1, 10);
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            int CountP = 0;
+            int CountN = 0;
+            int CountZ = 0;
+
+            int[] numbers = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    CountP++;
+                }
+                if (arr[i] % 2 != 0)
+                {
+                    CountN++;
+                }
+                if (!numbers.Contains(numbers[i]))
+                {
+                    numbers[CountZ] = arr[i];
+                    CountZ++;
+                }
+            }
+            Console.WriteLine($"Positiv: {CountP} Negativ: {CountN} Unique: {CountZ}");
+            */
+
+
+
+
+
             /*
             int num;
             Console.WriteLine("number: ");
@@ -37,13 +186,13 @@ namespace At_day__at_night
             */
 
             //7
-            int N;
-            Console.WriteLine("number: ");
-            N = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write(i);
-            }
+            // int N;
+            // Console.WriteLine("number: ");
+            // N = Convert.ToInt32(Console.ReadLine());
+            // for (int i = 0; i < N; i++)
+            // {
+            //     Console.Write(i);
+            // }
 
         }
 
